024a480ca38c6d3e90179f4393a9e04f1dd8eee1
Marco Mangan <marco.mangan@pucrs.br>
2025-05-19 12:15:37 -0300

    Initial sharing of project

---
+++ Hangman.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/**
+ * A hangman game.
+ * 
+ * @author marco.mangan@pucrs.br
+ * @date 2025-05-19
+ */
+public class Hangman
+{
+    /**
+     * 
+     */
+    private Hangman() { }
+    
+    /**
+     * 
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Jogo da Forca");
+        System.out.println("=============");
+        
+        String secret = "garrafa";
+        String hint =   "-------";
+
+        System.out.println(hint);
+
+        // Scanner in;
+        // int a;
+        // int b;
+        // int s;
+
+        // // entrada de dados
+        // in = new Scanner(System.in);
+        // a = in.nextInt();      
+        // b = in.nextInt();      
+        // in.close();
+
+        // // processamento de dados
+        // s = a + b;
+
+        // // saida de dados
+        // System.out.println(s);
+    }
+}
