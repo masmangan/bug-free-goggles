00023ffd85098a735e6e18f7e435a27f4dbfe184
Marco Mangan <marco.mangan@pucrs.br>
2025-05-23 12:05:32 -0300

    Closes #4

--- Hangman.java
+++ Hangman.java
@@ -13,45 +13,65 @@ public class Hangman
      * 
      */
     private Hangman() { }
-    
+
     /**
      * 
      */
     public static void main(String[] args)
     {
+        Scanner in;
         Random r = new Random();
-        
+        char guess;
+        String history = "";
+        int errors = 0;
+
         String[] secrets = {"garrafa", "elefante",
-        "suspeito", "banana", "carro"};
+                "suspeito", "banana", "carro"};
         int selected = r.nextInt(secrets.length);
-        
+
         System.out.println("Jogo da Forca");
         System.out.println("=============");
-        
+
         String secret = secrets[selected];
         String hint =   "";
         for (int i = 0; i < secret.length(); i++) {
             hint = hint + "-";
         }
 
-        System.out.println(secret);
-        System.out.println(hint);
+        in = new Scanner(System.in);
+        while (true) {
+            // bloco principal do jogo
+            System.out.printf("DEBUG: %s\n", secret);
+            System.out.printf("PALAVRA: %s\n", hint);
+            System.out.printf("TENTATIVAS: %s\n", history);
+            System.out.printf("ERROS: %d\n", errors);
+            
+            if (errors >= 6) {
+                break;
+            }
+            
+            System.out.print("Escolha uma letra: ");
+            String line = in.nextLine();
+            guess = line.charAt(0);
 
-        // Scanner in;
-        // int a;
-        // int b;
-        // int s;
+            System.out.printf("Voce escolheu: %c.\n", guess);
 
-        // // entrada de dados
-        // in = new Scanner(System.in);
-        // a = in.nextInt();      
-        // b = in.nextInt();      
-        // in.close();
+            if (history.contains(""+guess)) {
+                System.out.println("Voce ja escolheu essa letra antes!");
+            } else {
+                history = history + guess;
+                if (secret.contains(""+guess)) {
+                    System.out.println("Letra aparece em secret!");
+                    
+                } else {
+                    System.out.println("Voce errou!!");
+                    errors = errors + 1;
+                }
+            }
 
-        // // processamento de dados
-        // s = a + b;
-
-        // // saida de dados
-        // System.out.println(s);
+        }
+        //
+        System.out.println("Até logo!");
+        in.close();
     }
 }
